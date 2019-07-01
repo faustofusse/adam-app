61b7f2f3a4a894482499057ef89930092762d4f2
FUSSE FAUSTO <43719026@ort>
2019-07-01 14:25:33 -0300

    Fullscreen

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
     // Crea la ventana del navegador.
-    let win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+    while(true){
+        let win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            fullscreen: true,
+            autoHideMenuBar: true,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
 
-    // and load the index.html of the app.
-    win.loadFile('./views/login.html');
+        win.loadFile('./views/login.html');
+    }
 }
 
 app.on('ready', createWindow);
