c2f936fe3407cfb71ebcd0b1607f4fce570b9209
FUSSE FAUSTO <43719026@ort>
2019-07-03 13:54:27 -0300

    JS SpeechRecognition

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, systemPreferences } = require('electron')
 
 function createWindow() {
     // Crea la ventana del navegador.
     let win = new BrowserWindow({
         width: 800,
         height: 600,
+        //transparent: true,
+        //frame: false,
         //fullscreen: true,
         autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true
         }
     });
-    console.log('Hola');
+   
+    //console.log(systemPreferences.getMediaAccessStatus('microphone'));
+    /*systemPreferences.askForMediaAccess('microphone').then(value => {
+        if (value) console.log('Microphone granted.');
+    });*/
+    
     win.loadFile('./index.html');
 }
 
