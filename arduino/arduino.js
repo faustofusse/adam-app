873276cf5cb34cab478d95e355f9d629d877f915
Fausto Fusse <faustofusse@gmail.com>
2019-08-04 22:30:32 -0300

    no anda nada

--- arduino/arduino.js
+++ arduino/arduino.js
@@ -1,92 +1,105 @@
-var five = require("johnny-five");
-var board = new five.Board();
+module.exports = (io) => {
+    var five = require("johnny-five");
+    var board = new five.Board();
 
-board.on("ready", function () {
-    /**
-     * Motor A: PWM 11, dir 12
-     * Motor B: PWM 5, dir 4
-     */
-    var motors = new five.Motors([
-        { pins: { dir: 12, pwm: 11 }, invertPWM: true },
-        { pins: { dir: 4, pwm: 5 }, invertPWM: true }
-    ]);
+    board.on("ready", function () {
+        io.on('connection', socket => {
+            console.log('Usuario conectado.');
 
-    board.repl.inject({
-        motors: motors
+            socket.on('button-press', (data) => {
+                console.log('button-press: ' + data.button);
+                if (data.button === '1') {
+
+                } else if (data.button === '2') {
+
+                } else if (data.button === '3') {
+
+                } else if (data.button === '4') {
+
+                } else if (data.button === 'up') {
+
+                } else if (data.button === 'down') {
+
+                } else if (data.button === 'left') {
+
+                } else if (data.button === 'right') {
+
+                } else if (data.button === 'center') {
+
+                }
+            });
+
+            socket.on('button-release', (data) => {
+                console.log('button-release: ' + data.button);
+                if (data.button === '1') {
+
+                } else if (data.button === '2') {
+
+                } else if (data.button === '3') {
+
+                } else if (data.button === '4') {
+
+                } else if (data.button === 'up') {
+
+                } else if (data.button === 'down') {
+
+                } else if (data.button === 'left') {
+
+                } else if (data.button === 'right') {
+
+                } else if (data.button === 'center') {
+
+                }
+            });
+        });
+
+
+        /**
+         * Motor A: PWM 11, dir 12
+         * Motor B: PWM 5, dir 4
+         */
+        var motors = new five.Motors([
+            { pins: { dir: 12, pwm: 11 }, invertPWM: true },
+            { pins: { dir: 4, pwm: 5 }, invertPWM: true }
+        ]);
+
+        board.repl.inject({
+            motors: motors
+        });
+
+        // Go forward at full speed for 5 seconds
+        console.log("Full speed ahead!");
+        motors.forward(255);
+        board.wait(5000, function () {
+            motors.stop();
+        });
+
+        // Go backwards at full speed for 5 seconds
+        console.log("Now backwards!");
+        motors.reverse(255);
+        board.wait(5000, function () {
+            motors.stop();
+        });
+
+        // Go left...
+        console.log("To the left!");
+        motors[0].reverse(200);
+        motors[1].forward(200);
+        board.wait(5000, function () {
+            motors.stop();
+        });
+
+        // Go right...
+        console.log("To the right!");
+        motors[0].forward(200);
+        motors[1].reverse(200);
+        board.wait(5000, function () {
+            motors.stop();
+        });
+
+        // Use REPL if you want to go further
+        console.log("Done auto-driving! Use `motors` to control motors in REPL");
+
     });
 
-    // Go forward at full speed for 5 seconds
-    console.log("Full speed ahead!");
-    motors.forward(255);
-    board.wait(5000, function () {
-        motors.stop();
-    });
-
-    // Go backwards at full speed for 5 seconds
-    console.log("Now backwards!");
-    motors.reverse(255);
-    board.wait(5000, function () {
-        motors.stop();
-    });
-
-    // Go left...
-    console.log("To the left!");
-    motors[0].reverse(200);
-    motors[1].forward(200);
-    board.wait(5000, function () {
-        motors.stop();
-    });
-
-    // Go right...
-    console.log("To the right!");
-    motors[0].forward(200);
-    motors[1].reverse(200);
-    board.wait(5000, function () {
-        motors.stop();
-    });
-
-    // Use REPL if you want to go further
-    console.log("Done auto-driving! Use `motors` to control motors in REPL");
-
-});
-
-/*
-
-board.on("ready", function () {
-    console.log('BRUH THE BOARD IS READY');
-    var led = new five.Led(13);
-    led.blink(1500);
-
-    var motor = new five.Motor({
-        pins: {
-            pwm: 6,
-            dir: 7
-        },
-        invertPWM: true
-    });
-
-    motor.on("start", function () {
-        console.log("start", Date.now());
-    });
-
-    // set the motor going forward full speed
-    motor.forward(255);
-
-    // var proximity = new five.Proximity({
-    //     controller: "HCSR04",
-    //     pin: 7
-    // });
-
-    // proximity.on("data", function() {
-    //     console.log("Proximity: ");
-    //     console.log("  cm  : ", this.cm);
-    //     console.log("  in  : ", this.in);
-    //     console.log("-----------------");
-    // });
-
-    // proximity.on("change", function() {
-    //     console.log("The obstruction has moved.");
-    // });
-
-    this.repl.inject({ led, motor });
-});*/
+}
--- server.js
+++ server.js
@@ -3,22 +3,15 @@ const server = require('http').createServer();
 const io = require('socket.io')(server, {
   path: '/',
   serveClient: false,
-  // below are engine.IO options
   pingInterval: 10000,
   pingTimeout: 5000,
   cookie: false
 });
 
-io.on('connection', socket => {
-    console.log('Usuario conectado.');
+// const arduino = require('./arduino/arduino')(io);
 
-    socket.on('button-press', (data) => {
-        console.log('button-press: ' + data.button);
-    });
-    socket.on('button-release', (data) => {
-        console.log('button-release: ' + data.button);
-    });
-});
+var five = require("johnny-five");
+var board = new five.Board();
 
 server.listen(3000, () => {
     console.log('Servidor en el puerto 3000...');
