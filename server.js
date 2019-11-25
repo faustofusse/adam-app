43baf2afa14c3ba359a92c25929e1086f6a37c6d
Fausto Fusse <faustofusse@Faustos-MacBook-Pro.local>
2019-11-24 23:17:29 -0300

    Hola

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+// const ip = require('ip');
+// const sp = require('serialport')
+
+const io = require('socket.io')(server);
+io.on('connection', () => {
+  console.log('Usuario conectado');
+})
+
+const arduino = require('./arduino/arduino')(io);
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+  // res.send('hola')
+});
+
+server.listen(3000, async () => {
+    // await sp.list((err, ports) => {
+    //   if (err) throw err;
+    //   console.log(ports);
+    // });
+    // console.log('IP: ' + ip.address());
+    console.log('Servidor en el puerto 3000...');
+});
+
+module.exports = server;
